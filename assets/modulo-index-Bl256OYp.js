26b0037332f7adec7bab2cced99ed6fe13c8a87a
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2026-01-06 14:44:11 +0100

    Updates

--- assets/modulo-index-Bl256OYp.js
+++ assets/modulo-index-Bl256OYp.js
@@ -1,4 +1,4 @@
-import{i as a,a as t,b as o,g as r}from"./search-Dd5MUDFV.js";a();t();o();const c=document.querySelector("main"),d=parseInt(c?.dataset.modulo||"1",10);l(d);function l(s){const i=document.getElementById("classesGrid");if(!i)return;const n=r(s);if(n.length===0){i.innerHTML="<p>No hay clases disponibles</p>";return}i.innerHTML=n.map(e=>`
+import{i as a,a as t,b as o,g as r}from"./search-0qNoTBfR.js";a();t();o();const c=document.querySelector("main"),d=parseInt(c?.dataset.modulo||"1",10);l(d);function l(s){const i=document.getElementById("classesGrid");if(!i)return;const n=r(s);if(n.length===0){i.innerHTML="<p>No hay clases disponibles</p>";return}i.innerHTML=n.map(e=>`
     <a href="clase-${e.id}.html" class="class-nav-card">
       <div class="class-nav-number">${e.id}</div>
       <div class="class-nav-content">
