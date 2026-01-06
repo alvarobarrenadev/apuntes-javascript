3d6feeb744903449c282a23d3816693f92cae304
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2026-01-06 13:45:26 +0100

    Updates

--- assets/clase-DFH-gBSA.js
+++ assets/clase-DFH-gBSA.js
@@ -1,4 +1,4 @@
-import{c as h,i as u,a as f,b,g}from"./search-CN8YBVmN.js";function j(s,o){const i=document.getElementById("lessonContainer");if(!i)return;const c=h(s,o);if(!c){i.innerHTML='<p class="error">Clase no encontrada</p>';return}i.innerHTML=`
+import{c as h,i as u,a as f,b,g}from"./search-CM3emVmz.js";function j(s,o){const i=document.getElementById("lessonContainer");if(!i)return;const c=h(s,o);if(!c){i.innerHTML='<p class="error">Clase no encontrada</p>';return}i.innerHTML=`
     <article class="class-card">
       <div class="class-card-header">
         <div class="class-number">${c.id}</div>
